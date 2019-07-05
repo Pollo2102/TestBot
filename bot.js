42c0df245f74e069fbfe2aecf95eb50635d5ed15
Diego Valenzuela <valenv.dg@gmail.com>
2019-07-05 02:10:50 -0600

    Added song playback functionality

--- bot.js
+++ bot.js
@@ -1,3 +1,5 @@
+const {google} = require('googleapis');
+
 const Discord = require('discord.js');
 /* token.js is a file that contains the private key required to host the bot */
 const data = require('./token.json');
@@ -6,13 +8,22 @@ bot's functions to work properly */
 const config = require('./bot-config.json');
 const fs = require('fs');
 const client = new Discord.Client();
+const ytdl = require('ytdl-core'); // API used to reproduce audio
+const opus = require('node-opus');
 
 const token = data.token;
+const API_KEY = data.ytkey;
 
 let prefix = config.prefix;
 
+let youtube = google.youtube({
+  version: 'v3',
+  auth: `${API_KEY}`
+});
+
+
 client.on('ready', () => {
-  console.log("*****Console Log*****");
+  console.log('*****Console Log*****');
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
@@ -26,19 +37,19 @@ client.on('message', msg => {
   	return;
 
   //Lists all the available commands
-  if ((msg.content.startsWith(prefix + "help")) && (ArgCount(msg.content) == 1))
-  	msg.channel.send(`\`${prefix}ping - Pings the server\n${prefix}prefix - Changes the bot's prefix\n${prefix}oso - Bot helps you decide with a yes or no answer\n${prefix}time - Prints the current time\``);
+  if ((msg.content === (prefix + 'help')) && (ArgCount(msg.content) == 1))
+  	msg.channel.send(`\`${prefix}ping - Pings the server\n${prefix}prefix - Changes the bot's prefix\n${prefix}oso - Bot helps you decide with a yes or no answer\n${prefix}time - Prints the current time\n${prefix}play - Searches a song on Youtube and plays it on a Voice Channel\n${prefix}leave - Kicks the bot from its current voice channel\``);
 
   //Pings the bot and returns the latency in milliseconds
-  if ((msg.content.startsWith(prefix + "ping")) && (ArgCount(msg.content) == 1))
+  if ((msg.content === (prefix + 'ping')) && (ArgCount(msg.content) == 1))
   	msg.channel.send(`Bot ping is: \`${client.ping}ms\` :ping_pong:`);
 
   //Function that changes the bot's prefix through user input
-  if ((msg.content.startsWith(prefix + "prefix")) && (ArgCount(msg.content) == 2) && (msg.member.permissions.has("MANAGE_GUILD", 1))) {
+  if ((msg.content === (prefix + 'prefix')) && (ArgCount(msg.content) == 2) && (msg.member.permissions.has('MANAGE_GUILD', 1))) {
     let newPrefix = msg.content.split(" ").slice(1,2)[0];
     config.prefix = newPrefix;
 
-    fs.writeFile("./bot-config.json", JSON.stringify(config), (err) => {
+    fs.writeFile('./bot-config.json', JSON.stringify(config), (err) => {
       if (err) throw err;
       console.error;
     });
@@ -47,7 +58,7 @@ client.on('message', msg => {
   }
 
   //function that generates a random number and outputs a yes or no answer
-  if ((msg.content.startsWith(prefix + "oso")) && (ArgCount(msg.content) == 1)) {
+  if ((msg.content === (prefix + "oso")) && (ArgCount(msg.content) == 1)) {
     let randNumb = Math.floor((Math.random() * 100) + 1);
     
   	if (randNumb % 2 == 0)
@@ -57,10 +68,46 @@ client.on('message', msg => {
   }
 
   //Function that prints the current time.
-  if ((msg.content.startsWith(prefix + "time")) && (ArgCount(msg.content)) == 1) {
+  if ((msg.content === (prefix + "time")) && (ArgCount(msg.content)) == 1) {
     let currDate = new Date();
     msg.channel.send(`Current time is: ${currDate}`);
   }
+
+  if ((msg.content.startsWith(prefix + "play")) && (ArgCount(msg.content)) > 1) {
+    let searchParam = msg.content.split(' ');
+    searchParam.shift();
+    searchParam = searchParam.join(' ');
+
+    youtube.search.list({
+      part: 'snippet',
+      type: 'video',
+      q: `${searchParam}`,
+      order: 'viewCount',
+      maxResults: '10'
+    }, function (err, data) {
+      if (err) {
+        console.error(err);
+      }
+      if (data) {
+        //console.log(data.items[0].id.videoId);
+        results = data.data;  
+        
+        const streamOptions = { seek: 0, volume: 1 };
+        const vChannel = msg.member.voiceChannel;
+        vChannel.join()
+          .then(connection => {
+            const stream = ytdl(`https://www.youtube.com/watch?v=${data.data.items[0].id.videoId}`, { filter : 'audioonly' });
+            const dispatcher = connection.playStream(stream, streamOptions);
+          })
+          .catch(console.error);
+      }
+    });    
+  }
+
+  if ((msg.content === (prefix + "leave")) && (ArgCount(msg.content)))
+    msg.guild.me.setVoiceChannel(null);
+    
+
 
 });
 
@@ -77,5 +124,9 @@ function ArgCount(str) {
     return str.split(" ").length;
   }
 
+
+class parameters{
+  parameters(){};
+}
 
 client.login(token);
